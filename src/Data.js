d7e8aa94f59a7ddea26998e29053f916d1e50555
Sergio F. Rodriguez <serrod@gmail.com>
2020-01-07 16:02:18 +0100

    included List view

---
+++ src/Data.js
@@ -0,0 +1,49 @@
+export const managerData = [
+    3910, 3706, 2850, 4005, 3750, 2912, 3200, 3645, 4205, 3211, 3354, 3904
+];
+
+export const nationalAverageData = [
+    4010, 3600, 2900, 3550, 3800, 2900, 3000, 3500, 4000, 3700, 3550, 3800
+]
+
+export const managerQuarterData = [
+    500, 390, 670, 600, 780, 440, 600, 680, 550, 475, 700, 795
+]
+
+export const nationalAverageQuarterData = [
+    600, 400, 600, 550, 700, 500, 600, 700, 500, 550, 600, 700
+]
+
+
+//---Labels---//
+export const dirLabels = [ 
+                            "J1", 
+                            "J2", 
+                            "J3", 
+                            "J4", 
+                            "J5", 
+                            "J6", 
+                            "J8", 
+                            "CMD GROUP", 
+                            "SOHC", 
+                            "SPECIAL STAFF", 
+                            "JSOAC", 
+                            "JOG-E"
+                          ];
+
+export const trainingLabels = [
+                                "SHARP", 
+                                "Equal Opportunity and Prevention", 
+                                "Cyber Awarness", 
+                                "Suicide Prevention", 
+                                "AntiTerrorism", 
+                                "OPSEC", 
+                                "Armmy Subtance Abuse", 
+                                "SERE Code of Conduct", 
+                                "Cyber Security", 
+                                "Derivative Clasification", 
+                                "SERE Residence CRS", 
+                                "Other"
+                            ];
+//---Labels---//
+
---
+++ src/views/List/List.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import classes from "./List.module.css";
+import LineGraph from "../../components/List/LineGraph";
+import chartIcon from "../../assets/chart-icon.svg";
+import  { 
+            managerData, 
+            nationalAverageData, 
+            dirLabels, 
+            managerQuarterData, 
+            nationalAverageQuarterData, 
+            trainingLabels 
+        } from "../../Data";
+
+export default class List extends Component {
+    state = {
+        data: managerData,
+        average: nationalAverageData,
+        labels: dirLabels
+    }
+
+    handleButtonClick = e => {
+        const { value } = e.target;
+        const isAnnual = value === "annual";
+
+        const newData = isAnnual ? managerData : managerQuarterData;
+        const newLabels = isAnnual ? dirLabels : trainingLabels;
+        const newAverage = isAnnual ? nationalAverageData : nationalAverageQuarterData;
+
+        this.setState({
+            data: newData,
+            average: newAverage,
+            labels: newLabels
+        })
+    }
+
+    render() {
+        const { data, average, labels } = this.state;
+        return (
+            <div className={classes.container}>
+                <header>
+                    <img src={chartIcon} alt="bar chart icon" />
+                    <h1>Defense Ready Reporting List</h1>
+                </header>
+
+                <div className={classes.buttonContainer}>
+                    <button  value="annual" onClick={this.handleButtonClick} >
+                        Per Director
+                    </button>
+
+                    <button value="lastquarter" onClick={this.handleButtonClick} >
+                        Per Training
+                    </button>
+                </div>
+                <LineGraph data={data} average={average} labels={labels} />
+            </div>
+        )
+    }
+}
